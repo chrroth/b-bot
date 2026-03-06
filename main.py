01e49ea6245252412c8676eb54fc330874a76a9a
chrroth <roth.christoph@gmx.de>
2026-03-06 08:57:02 +0200

    added user input parsing

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 def main():
     load_dotenv()
@@ -12,9 +21,16 @@ def main():
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=messages
     )
 
+    if response.usage_metadata == None:
+        raise RuntimeError("No Meta Data")
+    elif args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
     print(response.text)
 
 
