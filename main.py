e1ecd6e58aea04bd05a12dfb96afda9023199e02
chrroth <roth.christoph@gmx.de>
2026-04-08 09:19:37 +0200

    added system prompt and function declaration

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import argparse
+from prompts import system_prompt
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from call_function import available_functions
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -21,7 +23,10 @@ def main():
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt,
+            temperature=0),
     )
 
     if response.usage_metadata == None:
@@ -30,8 +35,12 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
-    print(response.text)
+ 
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
